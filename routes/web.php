9cfa120a63af270b4e66df814add20736cf6a8b8
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-01-06 19:42:19 +0100

    add routing reference

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,62 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//REFERENCE
+
+// Route::get('/wsb', function () {
+//     return view('wsb', ['name' => 'Jacek', 'surname' => 'Cielecki']);
+// });
+
+// Route::get('pages/{x}', function($x) {
+//     $pages = [
+//         'about' => 'Strona WSB',
+//         'contact' => 'poznan@gmail.com',
+//         'home' => 'Strona domowa'
+//     ];
+//     return $pages[$x];
+// });
+
+// Route::get('address/{city?}/{street?}/{zipCode?}', function(String $city = " brak danych ",
+//  String $street = " - ", int $zipCode = null){
+//     if (is_null($zipCode)){
+//         $zipCode = " brak ";
+//     }else{
+//         $zipCode = substr($zipCode,0,2)."-".substr($zipCode,2,3);
+//     }
+//     echo <<<ADDRESS
+//     Kod pocztowy: $zipCode,
+//     Miasto: $city<br>
+//     Ulica: $street
+//     <hr>
+// ADDRESS;
+// }) -> name('address');
+
+// Route::redirect('/adres/{city?}/{street?}/{zipCode?}', '/address/{city?}/{street?}/{zipCode?}');
+
+// Route::prefix('admin')->group(function(){
+//     Route::get('/home/{name}', function(String $name){
+//         echo "Witaj $name na stronie administracyjnej";
+//     });
+
+//     Route::get('users', function(){
+//         echo "<h3>Użytkownicy systemu</h3>";
+//     });
+// });
+
+// Route::redirect('/admin/{name}', '/admin/home/{name}');
+
+// Route::get('/user/{name}/{age?}', function(String $name, int $age = null){
+//     echo "Imię: $name";
+//     if ($age !=null){
+//         echo "<br>Wiek: $age";
+//     }
+// })->where(['name' => '[A-Za-z]+', 'age' => '[0-9]+']);
+
+// Route::get('/car/{marka?}/{model?}/{kolor?}/{cena?}', function(String $marka = '...', String $model = '...', String $kolor = '...', String $cena = '...'){
+//     echo <<<CAR
+//     Marka: $marka, model: $model<br>
+//     Kolor: $kolor<br>
+//     Cena: $cena
+//     CAR;
+// })->where(['marka?' => '[A-Za-z]+', 'model?' => '[A-Za-z]+', 'kolor?' => '[A-Za-z]+', 'cena?' => '[A-Za-z]+']);
