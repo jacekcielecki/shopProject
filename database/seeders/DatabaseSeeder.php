40c000cbba8527ad376c1d266589f00d0f302fa0
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2023-01-07 13:18:33 +0100

    add listings from db as a reference

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listing::create([
+            'title' => 'testTitle1',
+            'tags' => 'testTag1',
+            'company' => 'testCompany1',
+            'location' => 'testLocation1',
+            'email' => 'testEmail1',
+            'website' => 'testWebsite1',
+            'description' => 'testDescription1',
+        ]);
+
+        Listing::create([
+            'title' => 'testTitle2',
+            'tags' => 'testTag2',
+            'company' => 'testCompany2',
+            'location' => 'testLocation2',
+            'email' => 'testEmail2',
+            'website' => 'testWebsite2',
+            'description' => 'testDescription2',
+        ]);
+
+        Listing::create([
+            'title' => 'testTitle3',
+            'tags' => 'testTag3',
+            'company' => 'testCompany3',
+            'location' => 'testLocation3',
+            'email' => 'testEmail3',
+            'website' => 'testWebsite3',
+            'description' => 'testDescription3',
+        ]);
     }
 }
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -6,5 +6,11 @@
 <a href="/listings/{{$listing['id']}}">
     <h2>{{$listing['title']}};</h2> 
 </a>
+    <p>{{$listing['tags']}}</p>
+    <p>{{$listing['company']}}</p>
+    <p>{{$listing['location']}}</p>
+    <p>{{$listing['email']}}</p>
+    <p>{{$listing['website']}}</p>
     <p>{{$listing['description']}}</p>
+
 @endforeach
